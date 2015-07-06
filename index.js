f82cdccce2d0117f64fbe621356aa9f4fb2da2ca
zoubin <zoubin04@gmail.com>
2015-07-06 17:28:21 +0800

    fix browserify-transform-tools

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * a clone from https://www.npmjs.com/package/graspify
+ *
+ */
 var grasp = require('grasp');
 var transformTools = require('browserify-transform-tools');
 
@@ -5,40 +9,15 @@ var options = {
 	jsFilesOnly: true
 };
 
-
-/**
- * String transform is called per-module.
- * In that, as many `require` calls you have, that many times it is called.
- * If you replace `require` call, transform for that module won’t be called.
- *
- * Transform can be called globally as `browserify -g graspify`.
- * In that case transform is called for each nested module as well,
- * getting global config, defined in package.json.
- *
- * `opts.config` is taken from package.json, can be whether object or array.
- * `opts.opts` is taken as browserify transform param.
- */
 module.exports = transformTools.makeStringTransform("graspify", options,
 	function (content, opts, done) {
 		try {
-			//normalize plain replacements
-			if (opts.opts && typeof opts.opts[0] === 'string') {
-				opts.opts = [opts.opts];
-			}
-			if (opts.config && typeof opts.config[0] === 'string') {
-				opts.config = [opts.config];
-			}
-
-			//merge opts & config for the full list of replacements
-			[].concat(opts.opts || [], opts.config || [])
-
-			//apply each replacement
-			.forEach(function(args){
-				var selectorType = args.length === 3 ? args.shift() : 'equery';
-				var selector = args[0];
-				var replacement = args[1];
-
-				content = grasp.replace(selectorType, selector, replacement, content);
+            var patterns = opts.opts && opts.opts.patterns || [];
+            if (typeof patterns[0] === 'string') {
+                patterns = [patterns];
+            }
+            patterns.forEach(function(args){
+				content = grasp.replace.apply(grasp, ['equery'].concat(args, content).slice(-4));
 			});
 
 			done(null, content);
--- test/case/deconsole.js
+++
@@ -1,7 +0,0 @@
-var x = console.x;
-console.time(1);
-var a = 1; console.log(a);
-console.timeEnd(1);
-console.log('final');
-console['log']('xyz');
-x();
